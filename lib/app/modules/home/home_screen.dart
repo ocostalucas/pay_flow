c231628a959569fbfb4ae96ffb2d2c138ad9e1f1
Lucas Costa <olucascosta98@gmail.com>
2021-06-22 23:19:47 -0300

    feat: Adiciona home

--- lib/app/modules/home/home_screen.dart
+++ lib/app/modules/home/home_screen.dart
@@ -1,10 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/app/shared/core/app_colors.dart';
+import 'package:pay_flow/app/shared/core/app_typography.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'home_controller.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final controller = HomeController();
+
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(color: Colors.blue),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              center: Alignment(0, 0.69),
+              focalRadius: 20,
+              colors: [Color(0xffFFC480), AppColors.primary],
+            ),
+          ),
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá, ",
+                    style: AppTypography.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Gabul",
+                          style: AppTypography.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: AppTypography.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: () {
+                print("Clicou");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
